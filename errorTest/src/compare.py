67f15bde1b27231395a15bea7fd37f229562f933
bstadt <brandonduderstadt@gmail.com>
2016-01-21 00:42:47 -0500

    finished error test script, waiting on data now

--- errorTest/src/compare.py
+++ errorTest/src/compare.py
@@ -10,6 +10,30 @@ from mayavi.mlab import *
 actual = np.load('../data/actual.npy')
 print actual.shape
 predicted = np.load('../data/predicted.npy')
-#set all mitochondria to detected
-actual[actual > 0] = 100
-predicted[predicted > 0] =100
+#set arrays to bools of either detected or nt
+actual[actual > 0] = 1
+predicted[predicted > 0] =1
+#initialize counter variables
+correct = 0
+falsePositive = 0
+falseNegative = 0
+groundTruthTrueVoxels = 0
+totalCheckedVoxels = 0
+for x in actual:
+	for y in x:
+		for z in y:
+			totalCheckedVoxels++
+			if(actual[x,y,z]):
+				groundTruthTrueVoxels++
+				if(predicted[x,y,z]):
+					correct++
+				else:
+					falseNegative++
+			if(predicted[x,y,z] and not actual[x,y,z]):
+				falsePositive++
+falseAlarmRate = falsePositive/(totalCheckedVoxels - groundTruthTrueVoxels)
+predictionRate = correct/groundTruthTrueVoxels
+missRate = falseNegative/(totalCheckedVoxels - groundTruthTrueVoxels)
+print 'far: ' + falseAlarmRate
+print 'pred: ' + predictionRate
+print 'miss: ' + missRate
